cba6bb56fc65a0111f59b80afa586b823bc9bc38
Andrey Ivanov <andrey-tm203@mail.ru>
2018-03-16 12:30:43 +0300

    make some correctings

--- src/js/progress.js
+++ src/js/progress.js
@@ -3,11 +3,22 @@ const initBar = container => {
   const scale = bar.querySelector('.orders__progress-fill');
   const perc = container.querySelector('.orders__progress-perc');
 
-  const percentage = bar.dataset.progress;
-  perc.textContent = `${percentage}%`;
+  const renderBar = percentage => {
+    perc.textContent = `${percentage}%`;
 
-  scale.style.backgroundImage = `linear-gradient(to right, red ${percentage}%, rgba(0, 0, 0, 0) ${percentage}%)`;
-  perc.style.left = `${percentage}%`;
+    scale.style.backgroundImage = `linear-gradient(to right, red ${percentage}%, rgba(0, 0, 0, 0) ${percentage}%)`;
+    perc.style.left = `${percentage}%`;
+  };
+
+  const mutationHandler = () => renderBar(bar.dataset.progress);
+
+  const observer = new MutationObserver(mutationHandler);
+  observer.observe(bar, {
+    attributes: true,
+    attributeFilter: ['data-progress']
+  });
+
+  mutationHandler();
 };
 
 export default initBar;
